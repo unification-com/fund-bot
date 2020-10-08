6d786cb567cf98ce07564ff5879b34b22becf79b
Indika Piyasena <indika.piyasena@gmail.com>
2020-10-08 22:12:14 +1000

    cleaner graphql request

--- src3/fundbot/crawl.py
+++ src3/fundbot/crawl.py
@@ -1,13 +1,23 @@
-import requests
+from python_graphql_client import GraphqlClient
 
-TOKEN_ADDRESS = '0xab2D2F5bc36620A57Ec4bB60D6A7Df2a847dEab5'
+XFUND_MARKET = '0xab2d2f5bc36620a57ec4bb60d6a7df2a847deab5'
 
 
 def uniswap_data():
-    payload = '{"query":"query CurrentTokenData($pairId: ID\u0021, $yesterday: Int\u0021) {\\n  pair(id: $pairId) {\\n    token0 {id symbol name}\\n    token1 {id symbol name}\\n    reserve0\\n    reserve1\\n    txCount\\n  }\\n  pairHourDatas(where: {hourStartUnix_gt: $yesterday, pair: $pairId}) {\\n    hourlyVolumeToken0\\n    hourlyVolumeToken1\\n    hourlyVolumeUSD\\n  }\\n  swaps(first: 1000, where: {pair: $pairId}, orderBy: timestamp, orderDirection: desc) {\\n    id\\n    pair {\\n      token0 {symbol}\\n      token1 {symbol}\\n    }\\n    transaction {id}\\n    timestamp\\n    sender\\n    amount0In\\n    amount1In\\n    amount0Out\\n    amount1Out\\n    amountUSD\\n    to\\n  }\\n}","variables":{"pairId":"0xab2d2f5bc36620a57ec4bb60d6a7df2a847deab5","yesterday":1602077532}}'
-    url = 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2'
-    r = requests.post(url, data=payload)
-    data = r.json()
+    client = GraphqlClient(
+        endpoint="https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2")
+    query = """
+     query CurrentTokenData($pairId: ID){
+          pair(id: $pairId)  {
+            token0 {id symbol name}
+            token1 {id symbol name}
+            reserve0
+            reserve1
+          }
+      }
+    """
+    variables = {"pairId": XFUND_MARKET}
+    data = client.execute(query=query, variables=variables)
     pooled_xfund = data['data']['pair']['reserve0']
     pooled_eth = data['data']['pair']['reserve1']
     return float(pooled_eth), float(pooled_xfund)
